cb1ade91cf47e716ac2bd7fd5824013636a49bc1
Tryvan <pich.trivan@institute.pse.ngo>
2024-12-20 16:12:32 +0700

    add

--- index.js
+++ index.js
@@ -4,15 +4,21 @@
 // Task 1: Comment Your JavaScript Code
 // Write a single-line comment below this line:
 
+/* This is a
+multi-line comment */
+
 // Task 2: Declare JavaScript Variables
 // Declare a variable named `myName`:
+
 
 // Task 3: Storing Values with the Assignment Operator
 // Assign the value "JavaScript" to a variable named `myLanguage`:
 
 // Task 4: Assigning the Value of One Variable to Another
 // Declare a variable named `a` with the value 5, and another variable `b`. Assign `a` to `b`:
-
+var a = 5;
+var a = b;
+a = b ;
 // Task 5: Initializing Variables with the Assignment Operator
 // Declare and initialize a variable named `myAge` with the value 25:
 
@@ -21,12 +27,19 @@
 
 // Task 7: Understanding Uninitialized Variables
 // Declare three variables `x`, `y`, and `z` without assigning values. Then, assign values 1, 2, and 3 to them:
+var a = 1;
+var b = 2;
+var c = 3;
 
 // Task 8: Understanding Case Sensitivity in Variables
 // Fix the variable names to follow case sensitivity:
 // let StUdEnTnAmE = "John";
 // let sTuDeNtAgE = 20;
+var StUdLyCapVaR;
+var TitleCaseOver;
 
+var StUdLyCapVaR = "John";
+var TitleCaseOver = 20;
 // Task 9: Explore Differences Between the var and let Keywords
 // Declare a variable using `var` and another using `let`. Show the difference in scope (use a block of code to demonstrate):
 
